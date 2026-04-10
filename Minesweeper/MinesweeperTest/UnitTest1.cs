1dfd9dff21e51171e586508c196ccb1ee1b8ddea
Rosie Gutierrez-Velasquez <GUTIERREZVER@ETSU.EDU>
2026-04-10 11:32:15 -0400

    Push

---
+++ Minesweeper/MinesweeperTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Minesweeper.Core;
+namespace MinesweeperTest;
+
+public class BoardTest
+{
+    [Fact]
+    public void BoardTesting()
+    {
+        // Arrange
+        Board board = new Board();
+        int rows = 5;
+        int cols = 5;
+        int mines = 5;
+        int seed = 42;
+        // Act
+        var board = new Board(rows, cols, mines, seed);
+        // Assert
+        Assert.Equal(mines, CountMines(board));
+    }
+}
